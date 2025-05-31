7e4a7ebc2ad2b5c395b6ce689c772a7002e86d26
brokenpike <brokenpike@garmr.org>
2025-05-31 19:13:06 +0200

    Enable Plasma6

--- configuration.nix
+++ configuration.nix
@@ -43,9 +43,10 @@
   hardware.graphics.enable = true;
   hardware.nvidia.open = false;
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
+  services.displayManager.sddm.enable = true;
+  #services.xserver.displayManager.gdm.enable = true;
+  #services.xserver.desktopManager.gnome.enable = true;
+  services.desktopManager.plasma6.enable = true;
   # Service fwupd
   services.fwupd.enable = true;
 
